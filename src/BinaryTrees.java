5a5edb9b992c124862e952bf174dfc75155f5c8c
Parimal Matte <pmprofessional26@gmail.com>
2024-09-27 18:14:55 +0530

    Binary Trees: flatten() commit

--- src/BinaryTrees.java
+++ src/BinaryTrees.java
@@ -266,6 +266,44 @@ public class BinaryTrees {
         }
     }
 
+    /*
+            Given the root of a binary tree, flatten the tree into a "linked list":
+            The "linked list" should use the same Node class where the right child pointer points to the next node
+            in the list and the left child pointer is always null.
+            The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+
+            Input: root = [1,2,5,3,4,null,6]
+            Output: [1,null,2,null,3,null,4,null,5,null,6]
+     */
+
+    private void helper(Node root, LinkedList<Node> list){
+        if(root != null){
+            list.add(root);
+            helper(root.left, list);
+            helper(root.right, list);
+            root.left = null;
+            root.right = null;
+        }
+    }
+
+    private void buildList(Node root, LinkedList<Node> list){
+        for(int x = 0; x < list.size(); x++){
+            root.right = list.get(x);
+            root = root.right;
+        }
+    }
+
+    public void flatten(Node root) {
+        if(root == null || (root.left == null && root.right == null)){
+            return;
+        }
+        else{
+            LinkedList<Node> list = new LinkedList<>();
+            helper(root, list);
+            buildList(root, list);
+            list = null;
+        }
+    }
 
 
 
