2b82750916533e1e45c8e7b76add7b23ecb07a5b
Parimal Matte <pmprofessional26@gmail.com>
2024-10-02 11:17:24 +0530

    Strings: romanToInteger() commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        int z = 0;
+        for(int x = 0; x < n; x++){
+            nums[x] = sc.nextInt();
 
+        }
+        for(int x = 0; x < n; x++){
+            if(nums[x] == 0){
+                z++;
+            }
+        }
+        int i = 0;
+        int j = 0;
+        while(z > 0 && i < n){
+            if(nums[i] != 0){
+                nums[j++] = nums[i];
+                z--;
+            }
+            i++;
+        }
+        while(i < n){
+            nums[i++] = 0;
+        }
+        for(int x = 0; x < n; x++){
+            System.out.print(nums[x] + " ");
+        }
     }
 
 }
--- src/Strings.java
+++ src/Strings.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 
 public class Strings {
 
@@ -38,6 +38,132 @@ public class Strings {
             }
             return ans.charAt(k - 1);
         }
+    }
+
+    /*
+            Given a Roman numeral, convert it to an integer. The Roman numerals are based on the following symbols and
+            their values I -> 1, V -> 5, X -> 10, L -> 50, C -> 100, D -> 500, M -> 1000, IV = 4 (5 - 1), IX = 9 (10 - 1),
+            XL = 40 (50 - 10), XC = 90 (100 - 10), CD = 400 (500 - 100), CM = 900 (1000 - 100)
+     */
+
+    private static class Result {
+        int value = 0;
+    }
+
+    private static void processVorX(Stack<Character> st, HashMap<Character, Integer> hm, Result result, char c){
+        if(st.isEmpty()){
+            st.push(c);
+        }
+        else{
+            char curr = st.pop();
+            if(curr == 'I'){
+                result.value += hm.get(c) - 1;
+            }
+            else{
+                st.push(curr);
+                st.push(c);
+            }
+        }
+    }
+
+    private static void processL(Stack<Character> st, HashMap<Character, Integer> hm, Result result, char c){
+        if(st.isEmpty()){
+            st.push(c);
+        }
+        else{
+            char curr = st.pop();
+            if(curr == 'X'){
+                result.value += hm.get(c) - 10;
+            }
+            else{
+                st.push(curr);
+                st.push(c);
+            }
+        }
+    }
+
+    private static void processC(Stack<Character> st, HashMap<Character, Integer> hm, Result result, char c){
+        if(st.isEmpty()){
+            st.push(c);
+        }
+        else{
+            char curr = st.pop();
+            if(curr == 'X'){
+                result.value += hm.get(c) - 10;
+            }
+            else{
+                st.push(curr);
+                st.push(c);
+            }
+        }
+    }
+
+    private static void processD(Stack<Character> st, HashMap<Character, Integer> hm, Result result, char c){
+        if(st.isEmpty()){
+            st.push(c);
+        }
+        else{
+            char curr = st.pop();
+            if(curr == 'C'){
+                result.value += hm.get(c) - 100;
+            }
+            else{
+                st.push(curr);
+                st.push(c);
+            }
+        }
+    }
+
+    private static void processM(Stack<Character> st, HashMap<Character, Integer> hm, Result result, char c){
+        if(st.isEmpty()){
+            st.push(c);
+        }
+        else{
+            char curr = st.pop();
+            if(curr == 'C'){
+                result.value += hm.get(c) - 100;
+            }
+            else{
+                st.push(curr);
+                st.push(c);
+            }
+        }
+    }
+
+    private static void romanToInteger() {
+        Scanner sc = new Scanner(System.in);
+        String roman = sc.next();
+        HashMap<Character, Integer> hm = new HashMap<>();
+        Stack<Character> st = new Stack<>();
+        Result result = new Result();
+        hm.put('I', 1);
+        hm.put('V', 5);
+        hm.put('X', 10);
+        hm.put('L', 50);
+        hm.put('C', 100);
+        hm.put('D', 500);
+        hm.put('M', 1000);
+        for (int x = 0; x < roman.length(); x++) {
+            char c = roman.charAt(x);
+            if (c == 'I') {
+                st.push(c);
+            } else if (c == 'V' || c == 'X') {
+                processVorX(st, hm, result, c);
+            } else if (c == 'L') {
+                processL(st, hm, result, c);
+            } else if (c == 'C') {
+                processC(st, hm, result, c);
+            } else if (c == 'D') {
+                processD(st, hm, result, c);
+            } else if (c == 'M') {
+                processM(st, hm, result, c);
+            }
+        }
+        while (!st.isEmpty()) {
+            result.value += hm.get(st.pop());
+        }
+
+        System.out.println(result.value);
     }
 
     public static void main(String[] args){
