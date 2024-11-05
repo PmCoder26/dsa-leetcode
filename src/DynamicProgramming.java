64ee6c2c15dc23c1afb4606b1b42493dbdad9733
Parimal Matte <pmprofessional26@gmail.com>
2024-11-05 20:58:41 +0530

    dynamic programming canJump()

--- src/DynamicProgramming.java
+++ src/DynamicProgramming.java
@@ -57,6 +57,59 @@ public class DynamicProgramming {
         }
     }
 
+    /*
+            You are given an integer array nums. You are initially positioned at the array's first index, and each
+            element in the array represents your maximum jump length at that position.
+            Return true if you can reach the last index, or false otherwise.
+
+            Example 1:
+                Input: nums = [2,3,1,1,4]
+                Output: true
+                Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+            Example 2:
+                Input: nums = [3,2,1,0,4]
+                Output: false
+                Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it
+                         impossible to reach the last index.
+     */
+
+    private boolean helper(int[] nums, int idx, int[] tabs){
+        if(idx >= nums.length - 1){
+            tabs[tabs.length - 1] = 1;
+            return true;
+        }
+        if(nums[idx] == 0){
+            tabs[idx] = -1;
+            return false;
+        }
+        if(tabs[idx] == -1){
+            return false;
+        }
+        else{
+            for(int x = nums[idx]; x > 0; x--){
+                if(helper(nums, idx + x, tabs)){
+                    tabs[idx] = 1;
+                    return true;
+                }
+                else if(x == 1){
+                    tabs[idx] = -1;
+                }
+            }
+            return false;
+        }
+    }
+
+    public boolean canJump(int[] nums) {
+        if(nums.length == 1 && nums[0] != 0){
+            return true;
+        }
+        else{
+            int[] tabs = new int[nums.length];
+            return helper(nums, 0, tabs);
+        }
+    }
+
     public static void main(String[] args) {
 
     }
