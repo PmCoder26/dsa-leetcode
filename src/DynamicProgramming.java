ab6ba6323c117f457ea2f2f362f6c2de4648a2ce
Parimal Matte <pmprofessional26@gmail.com>
2024-11-06 12:29:56 +0530

    dynamic programming: rob() commit

--- src/DynamicProgramming.java
+++ src/DynamicProgramming.java
@@ -110,6 +110,53 @@ public class DynamicProgramming {
         }
     }
 
+    /*
+            You are a professional robber planning to rob houses along a street. Each house has a certain amount of
+            money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have
+            security systems connected and it will automatically contact the police if two adjacent houses were broken
+            into on the same night.
+            Given an integer array nums representing the amount of money of each house, return the maximum amount of
+            money you can rob tonight without alerting the police.
+
+            Example 1:
+                Input: nums = [1,2,3,1]
+                Output: 4
+                Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+                Total amount you can rob = 1 + 3 = 4.
+            Example 2:
+                Input: nums = [2,7,9,3,1]
+                Output: 12
+                Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+                Total amount you can rob = 2 + 9 + 1 = 12.
+     */
+
+    private int helper(int[] nums, int[] tabs, int idx){
+        if(idx == nums.length - 1){
+            return tabs[idx] = nums[idx];
+        }
+        if(idx >= nums.length){
+            return 0;
+        }
+        if(tabs[idx] != -1){
+            return tabs[idx];
+        }
+        else{
+            int maxLoot = Integer.MIN_VALUE;
+            for(int x = idx; x < nums.length; x++){
+                maxLoot = Math.max(maxLoot, nums[x] + helper(nums, tabs, x + 2));
+            }
+            return tabs[idx] = maxLoot;
+        }
+    }
+
+    public int rob(int[] nums) {
+        int[] tabs = new int[nums.length];
+        for(int x = 0; x < tabs.length; x++){
+            tabs[x] = -1;
+        }
+        return helper(nums, tabs, 0);
+    }
+
     public static void main(String[] args) {
 
     }
