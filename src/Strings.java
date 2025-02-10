c80e792ffdb3202a42da394db365aab82d843721
Parimal Matte <123257338+PmCoder26@users.noreply.github.com>
2025-02-10 07:18:21 +0530

    Strings: clearDigits() commit

--- src/Strings.java
+++ src/Strings.java
@@ -166,6 +166,50 @@ public class Strings {
         System.out.println(result.value);
     }
 
+    /*
+            You are given a string s.
+
+            Your task is to remove all digits by doing this operation repeatedly:
+            Delete the first digit and the closest non-digit character to its left.
+            Return the resulting string after removing all digits.
+
+            Example 1:
+                Input: s = "abc"
+                Output: "abc"
+            Explanation:
+                There is no digit in the string.
+            Example 2:
+                Input: s = "cb34"
+                Output: ""
+            Explanation:
+                First, we apply the operation on s[2], and s becomes "c4".
+                Then we apply the operation on s[1], and s becomes "".
+     */
+
+    private static String clearDigits(String s) {
+        if(s.length() == 1) {
+            if(s.charAt(0) < '9' && s.charAt(0) > '0') {
+                return "";
+            }
+            return s;
+        }
+        else {
+            StringBuilder ans = new StringBuilder("");
+            for(int x = 0, y = -1; x < s.length(); x++) {
+                char c = s.charAt(x);
+                if(c >= 'a' && c <= 'z') {
+                    ans.append(c);
+                    y++;
+                }
+                else {
+                    ans.deleteCharAt(y);
+                    y--;
+                }
+            }
+            return ans.toString();
+        }
+    }
+
     public static void main(String[] args){
 
     }
