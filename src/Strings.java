cdcdfa56c3d111d49f8afb389865fc65d49d4352
Parimal Matte <123257338+PmCoder26@users.noreply.github.com>
2025-02-21 21:23:49 +0530

    Strings: countAndSay() commit

--- src/Strings.java
+++ src/Strings.java
@@ -287,8 +287,61 @@ public class Strings {
         }
     }
 
-    public static void main(String[] args){
+    /*
+            The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+                countAndSay(1) = "1"
+                countAndSay(n) is the run-length encoding of countAndSay(n - 1).
+            Run-length encoding (RLE) is a string compression method that works by replacing consecutive identical
+            characters (repeated 2 or more times) with the concatenation of the character and the number marking the
+            count of the characters (length of the run). For example, to compress the string "3322251" we replace "33"
+            with "23", replace "222" with "32", replace "5" with "15" and replace "1" with "11". Thus the compressed
+            string becomes "23321511".
 
+            Given a positive integer n, return the nth element of the count-and-say sequence.
+
+            Example 1:
+                Input: n = 4
+                Output: "1211"
+                Explanation:
+                    countAndSay(1) = "1"
+                    countAndSay(2) = RLE of "1" = "11"
+                    countAndSay(3) = RLE of "11" = "21"
+                    countAndSay(4) = RLE of "21" = "1211"
+
+            Example 2:
+                Input: n = 1
+                Output: "1"
+                Explanation:
+                    This is the base case.
+     */
+
+    public String countAndSay(int n) {
+        if(n == 1){
+            return "1";
+        }
+        else{
+            StringBuilder str = new StringBuilder(countAndSay(n - 1));
+            StringBuilder result = new StringBuilder();
+            int x = 1;
+            int count = 1;
+            char ele = str.charAt(0);
+            while(x < str.length()) {
+                char c = str.charAt(x);
+                if(c == ele) {
+                    count++;
+                }
+                else {
+                    result.append(count).append(ele);
+                    count = 1;
+                    ele = c;
+                }
+                x++;
+            }
+            result.append(count).append(ele);
+            return result.toString();
+        }
     }
+
+    public static void main(String[] args){}
 
 }
