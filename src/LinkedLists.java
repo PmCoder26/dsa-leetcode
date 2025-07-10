dd2866997d0843c414b066cfe9ccf50c312a077d
Parimal Matte <123257338+PmCoder26@users.noreply.github.com>
2025-07-10 11:06:26 +0530

    LinkedLists: flatten(DoublyNode head) commit

--- src/LinkedLists.java
+++ src/LinkedLists.java
@@ -11,6 +11,15 @@ public class LinkedLists {
         }
     }
 
+    private static class DoublyNode {
+        int data;
+        DoublyNode next, prev, child;
+        public DoublyNode(int data) {
+            this.data = data;
+            next = prev = child = null;
+        }
+    }
+
 
     // Que.1:
         /*
@@ -398,6 +407,69 @@ public class LinkedLists {
             curr = curr.next;
         }
         preCurr.next = null;
+    }
+
+    /*
+                                430. Flatten a Multilevel Doubly Linked List
+            You are given a doubly linked list, which contains nodes that have a next pointer, a previous pointer,
+            and an additional child pointer. This child pointer may or may not point to a separate doubly linked list,
+            also containing these special nodes. These child lists may have one or more children of their own,
+            and so on, to produce a multilevel data structure as shown in the example below.
+            Given the head of the first level of the list, flatten the list so that all the nodes appear in a
+            single-level, doubly linked list. Let curr be a node with a child list. The nodes in the child list
+            should appear after curr and before curr.next in the flattened list.
+            Return the head of the flattened list. The nodes in the list must have all of their child pointers set to null.
+            Example 1:
+                Input: head = [1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
+                Output: [1,2,3,7,8,11,12,9,10,4,5,6]
+                Explanation: The multilevel linked list in the input is shown.
+            Example 2:
+                Input: head = [1,2,null,3]
+                Output: [1,3,2]
+                Explanation: The multilevel linked list in the input is shown.
+     */
+
+    public DoublyNode flatten(DoublyNode head) {
+        if(head == null) {
+            return head;
+        }
+        else {
+            Stack<DoublyNode> st = new Stack<>();
+            DoublyNode curr = head;
+            while(curr != null) {
+                if(curr.child != null) {
+                    st.push(curr.next);
+                    if(curr.next != null) {
+                        curr.next.prev = null;
+                    }
+                    curr.next = curr.child;
+                    curr.child.prev = curr;
+                    curr.child = null;
+                    curr = curr.next;
+                }
+                else if(curr.next == null) {
+                    if(st.isEmpty()) {
+                        break;
+                    }
+                    else {
+                        DoublyNode temp = null;
+                        while(!st.isEmpty() && temp == null) {
+                            temp = st.pop();
+                        }
+                        if(temp == null) break;
+                        else {
+                            curr.next = temp;
+                            temp.prev = curr;
+                            curr = curr.next;
+                        }
+                    }
+                }
+                else {
+                    curr = curr.next;
+                }
+            }
+            return head;
+        }
     }
 
     public static void main(String[] args) {
