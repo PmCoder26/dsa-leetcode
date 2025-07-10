2f8f57874db66cb3d96f9f954511d952b435610d
Parimal Matte <123257338+PmCoder26@users.noreply.github.com>
2025-07-10 10:25:33 +0530

    LinkedLists: deleteNode() commit

--- src/LinkedLists.java
+++ src/LinkedLists.java
@@ -371,6 +371,35 @@ public class LinkedLists {
         }
     }
 
+    /*
+            There is a singly-linked list head and we want to delete a node node in it.
+            You are given the node to be deleted node. You will not be given access to the first node of head.
+            All the dataues of the linked list are unique, and it is guaranteed that the
+            given node 'node' is not the last node in the linked list.
+            Delete the given node. Note that by deleting the node, we do not mean removing it from memory.
+            We mean:
+                The dataue of the given node should not exist in the linked list.
+                The number of nodes in the linked list should decrease by one.
+                All the dataues before node should be in the same order.
+                All the dataues after node should be in the same order.
+           Example 1:
+               Input: head = [4,5,1,9], node = 5
+               Output: [4,1,9]
+               Explanation: You are given the second node with dataue 5, the linked list should become 4 -> 1 -> 9
+                            after calling your function
+     */
+
+    public void deleteNode(Node node) {
+        Node curr = node;
+        Node preCurr = curr;
+        while(curr.next != null) {
+            curr.data = curr.next.data;
+            preCurr = curr;
+            curr = curr.next;
+        }
+        preCurr.next = null;
+    }
+
     public static void main(String[] args) {
 
     }
