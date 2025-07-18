4bdb5a8ebcacd10b8deaa074b55899d6af37e3e2
Parimal Matte <123257338+PmCoder26@users.noreply.github.com>
2025-07-18 12:40:03 +0530

    BinaryTrees: flipEquiv() commit

--- src/BinaryTrees.java
+++ src/BinaryTrees.java
@@ -793,6 +793,56 @@ public class BinaryTrees {
         return null;
     }
 
+    /*
+            For a binary tree T, we can define a flip operation as follows:
+                choose any node, and swap the left and right child subtrees.
+            A binary tree X is flip equivalent to a binary tree Y if and only if we can make X equal to Y after some
+            number of flip operations.
+            Given the roots of two binary trees root1 and root2, return true if the two trees are flip equivalent or
+            false otherwise.
+
+            Example 1:
+                Flipped Trees Diagram
+                Input: root1 = [1,2,3,4,5,6,null,null,null,7,8], root2 = [1,3,2,null,6,4,5,null,null,null,null,8,7]
+                Output: true
+                Explanation: We flipped at nodes with values 1, 3, and 5.
+
+            Example 2:
+                Input: root1 = [], root2 = []
+                Output: true
+            Example 3:
+                Input: root1 = [], root2 = [1]
+                Output: false
+     */
+
+    public boolean flipEquiv(Node root1, Node root2) {
+        if(root1 == null && root2 == null) {
+            return true;
+        }
+        if(root1 == null || root2 == null || root1.data != root2.data) {
+            return false;
+        }
+        else {
+            Node left = root1.left;
+            Node right = root1.right;
+            // checking root1's left part with root2's right to swap.
+            boolean leftCondition = left != null && root2.right != null && left.data == root2.right.data;
+            // checking root1's right part with root2's left to swap.
+            boolean rightCondition = right != null && root2.left != null && right.data == root2.left.data;
+            // if leftCondition and the other swappable part is null.
+            // if rightCondition and the other swappable part is null.
+            boolean thirdCondition = (leftCondition && right == null && root2.left == null) || (rightCondition && left == null && root2.right == null);
+            // thirdCondition is taken because when one of the parts(left or right of the root)
+            // is null and one of the condition(leftCondition || rightCondition) is true,
+            // hence still swapping can be done.
+            if((leftCondition && rightCondition) || thirdCondition) {
+                root1.left = right;
+                root1.right = left;
+            }
+            return flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
+        }
+    }
+
     public static void main(String[] args) {}
 
 }
